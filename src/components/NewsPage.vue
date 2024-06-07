b02b638d813bee6f8686c5c7e3092340f88debff
Renatter <riko321riko321@gmail.com>
2024-06-07 18:34:58 +0500

    fix

--- src/components/NewsPage.vue
+++ src/components/NewsPage.vue
@@ -45,6 +45,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
 @media (max-width: 570px) {
   h1 {
    font-size: 15px;
